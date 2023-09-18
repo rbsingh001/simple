88301fbb093b4656aef9eae671d49a28dba9678e
rbsingh001 <robinhooda25@gmail.com>
2023-09-18 13:34:35 +0530

    deleting from li and localstorage

--- dom.js
+++ dom.js
@@ -1,29 +1,65 @@
-function myFunction(){
-    
-    
+function myFunction() {
     var n = document.getElementById('name').value;
     var e = document.getElementById('email').value;
 
-    if(n.trim() != '' && e.trim() !=''){
-
-        var users = JSON.parse(localStorage.getItem('users')) || [];
-
-        const user = {
-            name: n ,
-            email: e
-        };
-
-        users.push(user);
-        localStorage.setItem('users' , JSON.stringify(users));
-    }
-    var ul = document.getElementById('ul');
-    var li = document.createElement('li');
-    var t = document.createTextNode("Name: "+n+" "+"Email: "+e);
-    // console.log(t);
-    li.appendChild(t);
-    ul.appendChild(li);
     
+    if (n.trim() === '' || e.trim() === '') {
+        alert('Please enter both name and email.');
+        return; 
+    }
+
+    
+    var users = JSON.parse(localStorage.getItem('users')) || [];
+
+    const user = {
+        name: n,
+        email: e
+
+    };
+
+
+    users.push(user);
+
+    
+    localStorage.setItem('users', JSON.stringify(users));
+
+    
+
     
     document.getElementById('name').value = '';
     document.getElementById('email').value = '';
-}
+
+    
+    var ul = document.getElementById('ul');
+    var li = document.createElement('li');
+    var t = document.createTextNode("Name: " + n + " Email: " + e);
+
+    
+    var deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.onclick = function (n) {
+        
+        let ur = JSON.parse(localStorage.getItem('users'))
+        
+        for(let i=0;i<ur.length; i++){
+            if(ur[i].name == user.name ) {
+                console.log(i);
+                ur.splice(i, 1);
+           
+                localStorage.setItem('users', JSON.stringify(ur));
+                break;
+            }
+
+        }
+        
+        ul.removeChild(li);
+        
+    };
+
+    
+    li.appendChild(t);
+    li.appendChild(deleteButton);
+
+    
+    ul.appendChild(li);
+};
